1e14f87c24f59faca971d9a47afbdf5c328f3434
Érik Escobedo <erik@codigojade.com>
2015-10-28 18:54:07 -0600

    Create Skills table and model.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151028164624) do
+ActiveRecord::Schema.define(version: 20151028184830) do
+
+  create_table "skills", force: :cascade do |t|
+    t.string "name"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
