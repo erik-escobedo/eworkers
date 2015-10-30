5da34dfd047821a93b6438739ded96d3e151a4c9
Érik Escobedo <erik@codigojade.com>
2015-10-30 12:36:51 -0600

    Search layout

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   devise_for :workers, controllers: {
     registrations: :workers
   }
+
+  root 'search#show'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
